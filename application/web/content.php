0e55aff4cc08c5af31c802e0b73b325ce21cf32f
xXNoceboXx <quentin.belot.pro@gmail.com>
2016-05-14 14:44:10 +0200

    WUS 1/4

--- application/web/content.php
+++ application/web/content.php
@@ -25,9 +25,20 @@
 				require('html/login.html');
 			}
 		?>
+
 		<?php
 
-			include_once('ajax/controller.system_preference.php');
+			echo('<div class="functions">
+					<div class="home active"></div>
+					<div class="search"></div>
+				</div>');
+
+		?>
+
+
+		<?php
+
+			/*include_once('ajax/controller.system_preference.php');
 
 			if(getSystemPreference('mod_search')->mod_search == 1) {
 
@@ -41,7 +52,7 @@
 						</div>
 					</div>');
 
-			}
+			}*/
 		?>
 		<!--<div class="preferences">
 			<div class="title">
@@ -85,16 +96,20 @@
 			if(!isset($_SESSION['user_auth']) || $_SESSION['user_auth'] == '0') {
 				echo('<div class="medias">
 					<div class="title">
-						<label class="title">MEDIAS</label>
+						<label class="title">Mes abonnements</label>
 					</div>
 					<div class="data">');
 							require('websites.php');
 				echo('</div>
 					</div>');
+
+				echo('<div class="search-info">
+						<label class="info">Abonne-toi à tes médias préférés pour les voir s\'afficher dans ton d\'actualité en cliquant sur l\'icône <span>Home</span> rouge.</label>
+					</div>');
 			}
 		?>
 	</div>
-	<div class="articles">
+	<div class="articles active">
 
 		<?php 
 			if (session_status() == PHP_SESSION_NONE) {
@@ -120,6 +135,19 @@
 				echo('</div>');
 			}
 		?>
+	</div>
+	<div class='recherche'>
+
+		<div class='barre'>
+			<div class='texte'>
+				<input type="text" class='searched' placeholder="Recherche..."></input>
+			</div>
+			<div class='button'>
+				<input type="button" class='searching'></input>
+			</div>
+		</div>
+		<div class='resultat'></div>
+
 	</div>
 	<div class="informations">
 
@@ -157,12 +185,14 @@
 				if(getSystemPreference('mod_newsletter')->mod_newsletter == 1) {
 
 					echo('<div class="newsletter">
+							<div class="logo">
+							</div>
 							<div class="title">
-								<label>NEWSLETTER</label>
+								<label>INSCRIS-TOI A LA NEWSLETTER<br/>POUR NE RATER AUCUNE NEWS</label>
 							</div>
 							<div class="data">
 								<div class="input">
-									<input type="text" id="newsletter_email" placeholder="Saisissez votre adresse e-mail" />
+									<input type="text" id="newsletter_email" placeholder="Saisis ton adresse e-mail" />
 								</div>
 								<div class="button">
 									<a href="#" id="newsletter_valider" class="newsletter_valider"></a>
